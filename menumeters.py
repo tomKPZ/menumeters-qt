f0b441500449b8af08537740c1c8f3ad3e28e037
tomKPZ <tomKPZ@gmail.com>
2022-04-19 20:47:13 -0700

    stacked graphs

--- menumeters.py
+++ menumeters.py
@@ -3,23 +3,32 @@
 from PyQt5.QtCore import QTimer, QLineF
 from PyQt5.QtWidgets import QApplication, QSystemTrayIcon, QMenu, QAction
 from PyQt5.QtGui import QIcon, QPainter, QPixmap, QColorConstants
-import psutil
-import sys
+from psutil import virtual_memory, cpu_times
+from sys import argv, exit
+from time import monotonic
 
 
-def mem_percent():
-    mem = psutil.virtual_memory()
-    return (mem.total - mem.available) / mem.total
+def sample_mem():
+    mem = virtual_memory()
+    return (mem.total - mem.available, mem.available)
+
+
+def sample_cpu():
+    cpu = cpu_times()
+    return (cpu.system, cpu.user, cpu.idle)
 
 
 class TrayIcon(QSystemTrayIcon):
 
-    def __init__(self, parent, interval, color, get_usage):
+    def __init__(self, parent, width, height, interval, colors, sample):
         QSystemTrayIcon.__init__(self, parent)
 
-        self.get_usage = get_usage
-        self.color = color
-        self.usage = [0] * 32
+        self.width = width
+        self.height = height
+        self.colors = colors
+        self.sample = sample
+
+        self.window = [None] * width
         self.draw()
 
         right_menu = RightClicked()
@@ -32,18 +41,28 @@ class TrayIcon(QSystemTrayIcon):
         self.timer.start(interval)
 
     def timeout(self):
-        usage = self.get_usage()
-        self.usage = self.usage[1:] + [usage]
+        sample = self.sample()
+        # TODO: use circular buffer
+        self.window = self.window[1:] + [sample]
         self.draw()
 
     def draw(self):
-        pixmap = QPixmap(32, 32)
+        pixmap = QPixmap(self.width, self.height)
         pixmap.fill(QColorConstants.Transparent)
         with QPainter(pixmap) as painter:
-            painter.setPen(self.color)
             painter.setRenderHints(QPainter.Antialiasing)
-            for i, usage in enumerate(self.usage):
-                painter.drawLine(QLineF(i, 32 * (1 - usage), i, 32))
+            for i, sample in enumerate(self.window):
+                if sample is None:
+                    continue
+                total = sum(sample)
+                offset = 0
+                for val, color in zip(sample, self.colors):
+                    painter.setPen(color)
+                    height = val / total * self.height
+                    painter.drawLine(
+                        QLineF(i, self.height - offset, i,
+                               self.height - offset - height))
+                    offset = height
         icon = QIcon(pixmap)
         self.setIcon(icon)
 
@@ -58,11 +77,30 @@ class RightClicked(QMenu):
         self.addAction(action)
 
 
+class DeltaSampler():
+
+    def __init__(self, sampler):
+        self.sampler = sampler
+        self.prev = self.sampler()
+        self.prev_ts = monotonic()
+
+    def __call__(self):
+        sample = self.sampler()
+        ts = monotonic()
+        delta = [(s2 - s1) / (self.prev_ts - ts)
+                 for (s1, s2) in zip(self.prev, sample)]
+        self.prev, self.prev_ts = sample, ts
+        return delta
+
+
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
+    app = QApplication(argv)
 
-    cpu = TrayIcon(app, 100, QColorConstants.Cyan,
-                   lambda: psutil.cpu_percent() / 100)
-    mem = TrayIcon(app, 100, QColorConstants.Green, mem_percent)
+    cpu = TrayIcon(app, 32, 32, 100,
+                   (QColorConstants.Blue, QColorConstants.Cyan,
+                    QColorConstants.Transparent), DeltaSampler(sample_cpu))
+    mem = TrayIcon(app, 32, 32, 100,
+                   (QColorConstants.Green, QColorConstants.Transparent),
+                   sample_mem)
 
-    sys.exit(app.exec_())
+    exit(app.exec_())
